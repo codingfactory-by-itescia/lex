2eab4c8e239615ed63824ff31a189ad1ac65ca98
aymerickvalette <valaymerick@gmail.com>
2018-01-28 17:18:44 +0100

    Add proof validation

--- core/proof.py
+++ core/proof.py
@@ -1,18 +1,22 @@
 from hashlib import sha256
 
+
 class Proof:
     def __init__(self, block):
         self.block = block
-        self.data = block.previous_hash + block.data + block.timestamp
-        self.nounce = 0
-        self.hash = sha256((self.data + str(self.nounce)).encode()).hexdigest()
-        self.run()
 
     def run(self):
-        while self.hash[:4] != "0000":
-            print(self.hash)
+        self.data = self.block.previous_hash + self.block.data + self.block.timestamp
+        self.nounce = 0
+        self.hash = sha256((self.data + str(self.nounce)).encode()).hexdigest()
+
+        while self.hash[:2] != "00":
             self.nounce += 1
-            self.hash = sha256((self.data + str(self.nounce)).encode()).hexdigest()
+            self.hash = sha256(
+                (self.data + str(self.nounce)).encode()).hexdigest()
 
-
-
+    def validate(self):
+        current_hash = sha256(((self.block.previous_hash + self.block.data + self.block.timestamp) + str(self.block.nounce)).encode()).hexdigest()
+        if self.block.hash == current_hash:
+            return True
+        return False
