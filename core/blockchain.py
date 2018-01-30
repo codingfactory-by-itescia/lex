e100b99198f022c88fe6f2270a733aa80d1c37c8
aymerickvalette <valaymerick@gmail.com>
2018-01-31 00:01:55 +0100

    Convert Array storage to database storage

--- core/blockchain.py
+++ core/blockchain.py
@@ -5,24 +5,31 @@ from pymongo import MongoClient
 class Blockchain:
 
     def __init__(self):
-        self.blocks = []
-        self.create_genesis()
+        client = MongoClient()
+        self.database = client.blockchain
+
+        if self.database.blocks.count() == 0:
+            self.create_genesis()
 
     def add_block(self, data):
-        previous_block = self.blocks[len(self.blocks)-1]
-        new_block = Block(data, previous_block.hash)
-        self.blocks.append(new_block)
-
-        client = MongoClient()
-        db = client.blockchain 
-        db.blocks.insert_one({
+        previous_block = self.database.blocks.find_one({"tip":"true"})
+        new_block = Block(data, previous_block['hash'])
+        self.database.blocks.insert_one({
             "timestamp": new_block.timestamp,
             "data": new_block.data,
             "hash": new_block.hash,
             "previous_hash": new_block.previous_hash,
-            "nounce": new_block.nounce
+            "nounce": new_block.nounce,
+            "tip":"true"
         })
 
     def create_genesis(self):
         new_genesis = Block("Genesis block", "0")
-        self.blocks.append(new_genesis)
+        self.database.blocks.insert_one({
+            "timestamp": new_genesis.timestamp,
+            "data": new_genesis.data,
+            "hash": new_genesis.hash,
+            "previous_hash": new_genesis.previous_hash,
+            "nounce": new_genesis.nounce,
+            "tip":"true"
+        })
