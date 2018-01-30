d822e41a1262f059cfd267d040f3918002b752f6
Aymerick Valette <aymerick.valette@adotmob.com>
2018-01-30 13:45:29 +0100

    Store new blocks in MongoDB

--- core/blockchain.py
+++ core/blockchain.py
@@ -1,4 +1,5 @@
 from block import Block
+from pymongo import MongoClient
 
 
 class Blockchain:
@@ -11,6 +12,16 @@ class Blockchain:
         previous_block = self.blocks[len(self.blocks)-1]
         new_block = Block(data, previous_block.hash)
         self.blocks.append(new_block)
+
+        client = MongoClient()
+        db = client.blockchain 
+        db.blocks.insert_one({
+            "timestamp": new_block.timestamp,
+            "data": new_block.data,
+            "hash": new_block.hash,
+            "previous_hash": new_block.previous_hash,
+            "nounce": new_block.nounce
+        })
 
     def create_genesis(self):
         new_genesis = Block("Genesis block", "0")
