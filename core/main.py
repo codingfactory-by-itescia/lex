13967b5f4caaae654100b16e54e86faf4ba03a06
aymerickvalette <valaymerick@gmail.com>
2018-01-31 00:02:20 +0100

    Delete print debug lines

--- core/main.py
+++ core/main.py
@@ -4,14 +4,7 @@ from proof import Proof
 
 def main():
     chain = blockchain.Blockchain()
-    chain.add_block("Bonjour")
-    for b in chain.blocks:
-        print("Data : " + b.data)
-        print("Hash : " + b.hash)
-        print("Previous_hash : " + b.previous_hash)
-        print("Proof : " + str(b.nounce))
-        proof = Proof(b)
-        print(proof.validate())
+    chain.add_block("Hello World")
 
 
 if __name__ == '__main__':
