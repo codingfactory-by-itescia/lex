cc54a8c1aee27cae8887e1b32af9c9a2fa681378
aymerickvalette <valaymerick@gmail.com>
2018-02-01 23:40:55 +0100

    Add chain iterator

---
+++ core/iterator.py
@@ -0,0 +1,8 @@
+class Iterator:
+    def __init__(self, db, current_hash):
+        self.db = db
+        self.current_hash = current_hash
+
+    def next(self):
+        current = self.db.find_one({hash: self.current_hash})
+        return self.db.find_one({hash: current.previous_hash})
