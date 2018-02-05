ff9267614dbc67f1f2409ab325672381b61f34c4
aymerickvalette <valaymerick@gmail.com>
2018-02-05 21:15:32 +0100

    Show all blockchain from the tip to the genesis

--- core/cli.py
+++ core/cli.py
@@ -1,19 +1,40 @@
 import argparse
 
 from blockchain import Blockchain
+from iterator import Iterator
 
-parser = argparse.ArgumentParser()
-parser.add_argument("command", help="type of operation: add <data> or show")
-parser.add_argument("--data", help="block data")
+class CLI:
+    def run(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("command", help="type of operation: add <data> or show")
+        parser.add_argument("--data", help="block data")
 
-args = parser.parse_args()
+        args = parser.parse_args()
 
-blockchain = Blockchain()
+        blockchain = Blockchain()
 
-if args.data: 
-    if args.command == "add":
-        blockchain.add_block(args.data)
-        print("Done")
+        if args.data: 
+            if args.command == "add":
+                blockchain.add_block(args.data)
+                print("Done")
 
-if args.command == "show":
-    print("blockchain")
+        if args.command == "show":
+            iterator = blockchain.iterator()
+        
+
+            while True:
+                block = iterator.next()
+
+                print("previous_hash : " + block['previous_hash'])
+                print("current_hash : " + block['hash'])
+                print("data : " + block['data'])
+                print("timestamp : " + block['timestamp'])
+                print("nounce : " + str(block['nounce']))
+                print('\n')
+
+                if block['previous_hash'] == "0":
+                    break
+                
+                
+                
+
