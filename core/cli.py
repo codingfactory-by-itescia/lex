9fca5eba0c5fd68149aa50e5cf8ebbb2e4e6364a
aymerickvalette <valaymerick@gmail.com>
2018-02-05 20:12:52 +0100

    Edit help text

--- core/cli.py
+++ core/cli.py
@@ -3,7 +3,7 @@ import argparse
 from blockchain import Blockchain
 
 parser = argparse.ArgumentParser()
-parser.add_argument("command", help="type of operation: add <data> or display")
+parser.add_argument("command", help="type of operation: add <data> or show")
 parser.add_argument("--data", help="block data")
 
 args = parser.parse_args()
