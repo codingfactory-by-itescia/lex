135213a610bf1528ca6758d9f68d274ba8dfd877
aymerickvalette <valaymerick@gmail.com>
2018-02-05 19:57:52 +0100

    Parse arguments to create blocks

--- core/cli.py
+++ core/cli.py
@@ -1,11 +1,19 @@
 import argparse
+
 from blockchain import Blockchain
 
-parser = argparse.ArgumentParser(description='Creates a block')
-
-parser.add_argument("addblock", metavar='command', help='')
+parser = argparse.ArgumentParser()
+parser.add_argument("command", help="type of operation: add <data> or display")
+parser.add_argument("--data", help="block data")
 
 args = parser.parse_args()
 
-chain = Blockchain()
-chain.add_block(args)
+blockchain = Blockchain()
+
+if args.data: 
+    if args.command == "add":
+        blockchain.add_block(args.data)
+        print("Done")
+
+if args.command == "show":
+    print("blockchain")
